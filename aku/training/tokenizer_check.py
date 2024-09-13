59c2c21498abd6e37e7b9ce07891bba561085907
YukiTomita-CC <tomita_y@clowncolor.com>
2024-09-13 18:17:42 +0900

    fix

--- aku/training/tokenizer_check.py
+++ aku/training/tokenizer_check.py
@@ -3,38 +3,38 @@ from transformers import T5Tokenizer
 
 tokenizer = T5Tokenizer.from_pretrained("models/inc_batch_2epoch")
 
-# print("INFO: Check the special tokens")
-# check_tokens = {
-#     "bos_token": tokenizer.bos_token,
-#     "eos_token": tokenizer.eos_token,
-#     "pad_token": tokenizer.pad_token,
-#     "unk_token": tokenizer.unk_token,
-#     "context_begin_token": "<CONTEXT>",
-#     "context_end_token": "</CONTEXT>",
-#     "role_begin_token": "<ROLE>",
-#     "role_end_token": "</ROLE>",
-#     "line_break_token": "\n"
-# }
+print("INFO: Check the special tokens")
+check_tokens = {
+    "bos_token": tokenizer.bos_token,
+    "eos_token": tokenizer.eos_token,
+    "pad_token": tokenizer.pad_token,
+    "unk_token": tokenizer.unk_token,
+    "context_begin_token": "<CONTEXT>",
+    "context_end_token": "</CONTEXT>",
+    "role_begin_token": "<ROLE>",
+    "role_end_token": "</ROLE>",
+    "line_break_token": "\n"
+}
 
-# print (f"| Token ID | {'Token Name':<19} | {'String':<10} |")
-# print("|----------|---------------------|------------|")
-# for token_name, token_string in check_tokens.items():
-#     print(f"| {tokenizer.convert_tokens_to_ids(token_string):<8} | {token_name:<19} | {token_string:<10} |")
-# print()
+print (f"| Token ID | {'Token Name':<19} | {'String':<10} |")
+print("|----------|---------------------|------------|")
+for token_name, token_string in check_tokens.items():
+    print(f"| {tokenizer.convert_tokens_to_ids(token_string):<8} | {token_name:<19} | {token_string:<10} |")
+print()
 
 
-# print("INFO: Check the vocabulary size")
-# print(f"Vocabulary size: {tokenizer.vocab_size}")
-# print()
+print("INFO: Check the vocabulary size")
+print(f"Vocabulary size: {tokenizer.vocab_size}")
+print()
 
 
 print("INFO: Check the encoding and decoding of some examples")
 examples = [
-    # "Hello, my name is John.",
-    # "I am a student.",
-    # "こんにちは、私の名前はAkuです。",
-    # "私は学生です。",
-    # "<BOS><CONTEXT>Akuは社会人として働いていて、趣味は音楽です。</CONTEXT>\n\n<ROLE>User</ROLE>\n趣味は何ですか?\n<ROLE>Aku</ROLE>\nこんにちは。音楽が好きです。<EOS>",
+    "Hello, my name is John.",
+    "I am a student.",
+    "こんにちは、私の名前はAkuです。",
+    "私は学生です。",
+    "<BOS><CONTEXT>Akuは社会人として働いていて、趣味は音楽です。</CONTEXT>\n\n<ROLE>User</ROLE>\n趣味は何ですか?\n<ROLE>Aku</ROLE>\nこんにちは。音楽が好きです。<EOS>",
     "<CONTEXT>*UserとAkuは家で二人でのんびりしている</CONTEXT>\n\n<ROLE>User</ROLE>\n来週の土日でプチ旅行しない? <ATTR> likability: 5 mood: 5 </ATTR>\n<ROLE>Aku</ROLE>\n"
 ]
 
@@ -49,15 +49,15 @@ for example in examples:
     print("-"*50)
 
 
-# print("INFO: Check the encoding and decoding of some emojis")
-# emoji_list = ["😊", "🥰", "😉", "🤗", "😭", "🤣", "🥺", "💓", "✨"]
+print("INFO: Check the encoding and decoding of some emojis")
+emoji_list = ["😊", "🥰", "😉", "🤗", "😭", "🤣", "🥺", "💓", "✨"]
 
-# for emoji in emoji_list:
-#     print(f"Emoji: {emoji}")
-#     encoded_emoji = tokenizer.encode(emoji)
-#     print(f"Encoded: {encoded_emoji}")
-#     print(f"Decoded: {tokenizer.decode(encoded_emoji)}")
-#     print("-"*50)
+for emoji in emoji_list:
+    print(f"Emoji: {emoji}")
+    encoded_emoji = tokenizer.encode(emoji)
+    print(f"Encoded: {encoded_emoji}")
+    print(f"Decoded: {tokenizer.decode(encoded_emoji)}")
+    print("-"*50)
 
 print("INFO: Check the decoding of some examples")
 examples = [
