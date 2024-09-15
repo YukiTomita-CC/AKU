af5913519a047893769f7b67adac8786d23cc267
YukiTomita-CC <tomita_y@clowncolor.com>
2024-09-15 13:24:56 +0000

    edit tokenizer sctipts

--- aku/training/tokenizer_check.py
+++ aku/training/tokenizer_check.py
@@ -1,7 +1,8 @@
-from transformers import T5Tokenizer
+from transformers import AutoTokenizer
 
 
-tokenizer = T5Tokenizer.from_pretrained("models/inc_batch_2epoch")
+tokenizer = AutoTokenizer.from_pretrained("models/custom_llama_tokenizer", use_fast=False)
+print("Type: ", type(tokenizer))
 
 print("INFO: Check the special tokens")
 check_tokens = {
@@ -9,17 +10,25 @@ check_tokens = {
     "eos_token": tokenizer.eos_token,
     "pad_token": tokenizer.pad_token,
     "unk_token": tokenizer.unk_token,
+    "sep_token": "<SEP>",
     "context_begin_token": "<CONTEXT>",
     "context_end_token": "</CONTEXT>",
     "role_begin_token": "<ROLE>",
     "role_end_token": "</ROLE>",
-    "line_break_token": "\n"
+    "user_token": "User",
+    "aku_token": "Aku",
+    "attribute_begin_token": "<ATTR>",
+    "attribute_end_token": "</ATTR>",
+    "likability_token": "likability",
+    "mood_token": "mood",
+    "line_break_token": "\n",
+    "whitespace_token": " ",
 }
 
-print (f"| Token ID | {'Token Name':<19} | {'String':<10} |")
-print("|----------|---------------------|------------|")
+print (f"| Token ID | {'Token Name':<21} | {'String':<10} |")
+print("|----------|-----------------------|------------|")
 for token_name, token_string in check_tokens.items():
-    print(f"| {tokenizer.convert_tokens_to_ids(token_string):<8} | {token_name:<19} | {token_string:<10} |")
+    print(f"| {tokenizer.convert_tokens_to_ids(token_string):<8} | {token_name:<21} | {token_string:<10} |")
 print()
 
 
@@ -34,23 +43,23 @@ examples = [
     "I am a student.",
     "こんにちは、私の名前はAkuです。",
     "私は学生です。",
-    "<BOS><CONTEXT>Akuは社会人として働いていて、趣味は音楽です。</CONTEXT>\n\n<ROLE>User</ROLE>\n趣味は何ですか?\n<ROLE>Aku</ROLE>\nこんにちは。音楽が好きです。<EOS>",
+    "<CONTEXT>Akuは社会人として働いていて、趣味は音楽です。</CONTEXT>\n\n<ROLE>User</ROLE>\n趣味は何ですか?\n<ROLE>Aku</ROLE>\nこんにちは。音楽が好きです。<EOS>",
     "<CONTEXT>*UserとAkuは家で二人でのんびりしている</CONTEXT>\n\n<ROLE>User</ROLE>\n来週の土日でプチ旅行しない? <ATTR> likability: 5 mood: 5 </ATTR>\n<ROLE>Aku</ROLE>\n"
 ]
 
 for example in examples:
     print(f"Example: {example}")
-    encoded_example = tokenizer.encode(example, add_special_tokens=False)
+    encoded_example = tokenizer.encode(example)
     print(f"Encoded: {encoded_example}")
     print(f"Decoded: ", end="")
     for token_id in encoded_example:
-        print(tokenizer.decode([token_id]), end="/")
+        print(tokenizer.decode([token_id]), end="|")
     print("")
     print("-"*50)
 
 
 print("INFO: Check the encoding and decoding of some emojis")
-emoji_list = ["😊", "🥰", "😉", "🤗", "😭", "🤣", "🥺", "💓", "✨"]
+emoji_list = ["😊", "🥰", "😉", "🤗", "😭", "🤣", "😆", "🙄", "😡", "😲", "😳", "😔", "😇", "😙", "🥳", "🤔", "🥺", "🥱", "💓", "✨"]
 
 for emoji in emoji_list:
     print(f"Emoji: {emoji}")
@@ -61,11 +70,10 @@ for emoji in emoji_list:
 
 print("INFO: Check the decoding of some examples")
 examples = [
-    [308, 832, 285, 16687, 2011, 1196, 19136, 328, 370, 6324, 5519, 1376, 11, 3],
-    [308, 27403, 342, 438, 1326, 286, 5571, 299, 388, 1826, 1376, 3]
+    [10]
 ]
 
 for example in examples:
     print(f"Example: {example}")
-    print(f"Decoded: {tokenizer.decode(example, skip_special_tokens=True)}")
+    print(f"Decoded: {tokenizer.decode(example).encode('unicode-escape')}")
     print("-"*50)
--- aku/training/tokenizer_train.py
+++ aku/training/tokenizer_train.py
@@ -1,70 +1,112 @@
 import os
 
 import sentencepiece as spm
-from transformers import T5Tokenizer
+from transformers import LlamaTokenizer, T5Tokenizer
 
 
-MODEL_PREFIX = "sentencepice_model"
-OUTPUT_DIR = "output/custom_tokenizer5"
+def train_tokenizer(corpus_file: str="corpus.txt", save_as_llama_tokenizer: bool=True, save_as_t5_tokenizer: bool=True):
+    MODEL_PREFIX = "models/sentencepiece_model"
+    OUTPUT_DIR_1 = "models/custom_llama_tokenizer"
+    OUTPUT_DIR_2 = "models/custom_t5_tokenizer"
 
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+    spm.SentencePieceTrainer.train(
+        input=corpus_file,
+        model_type="unigram",
+        model_prefix=MODEL_PREFIX,
+        vocab_size=32000,
+        accept_language=["ja", "en"],
+        character_coverage=0.9995,
+        user_defined_symbols=user_defined_symbols,
+        byte_fallback=True,
+        add_dummy_prefix=False,
+        allow_whitespace_only_pieces=True,
+        unk_id=3,
+        bos_id=0,
+        eos_id=1,
+        pad_id=2,
+        unk_piece="<UNK>",
+        bos_piece="<BOS>",
+        eos_piece="<EOS>",
+        pad_piece="<PAD>",
+        train_extremely_large_corpus=True,
+        )
 
-user_defined_symbols = [
-    "<CONTEXT>",
-    "</CONTEXT>",
-    "<ROLE>",
-    "</ROLE>",
-    "User",
-    "Aku",
-    "😊",
-    "🥰",
-    "😉",
-    "🤗",
-    "😭",
-    "🤣",
-    "🥺",
-    "😆",
-    "🙄",
-    "😡",
-    "😲",
-    "😳",
-    "😔",
-    "💓",
-    "✨",
-    "<ATTR>",
-    "</ATTR>",
-    "likability",
-    "mood"
-]
+    if save_as_llama_tokenizer:
+        i = 1
+        while os.path.exists(OUTPUT_DIR_1):
+            OUTPUT_DIR_1 = f"models/custom_llama_tokenizer_{i}"
+            i += 1
+        os.makedirs(OUTPUT_DIR_1)
 
-spm.SentencePieceTrainer.train(
-    input="corpus.txt",
-    model_type="unigram",
-    model_prefix=MODEL_PREFIX,
-    vocab_size=32000,
-    accept_language=["ja", "en"],
-    character_coverage=0.9995,
-    user_defined_symbols=user_defined_symbols,
-    byte_fallback=True,
-    add_dummy_prefix=False,
-    unk_id=3,
-    bos_id=0,
-    eos_id=1,
-    pad_id=2,
-    unk_piece="<UNK>",
-    bos_piece="<BOS>",
-    eos_piece="<EOS>",
-    pad_piece="<PAD>"
-    )
+        tokenizer1 = LlamaTokenizer(
+            vocab_file=MODEL_PREFIX+".model",
+            unk_token="<UNK>",
+            bos_token="<BOS>",
+            eos_token="<EOS>",
+            pad_token="<PAD>",
+            add_bos_token=True,
+            add_eos_token=False,
+            clean_up_tokenization_spaces=False,
+            legacy=False
+        )
 
-tokenizer = T5Tokenizer(
-    vocab_file=MODEL_PREFIX+".model",
-    unk_token="<UNK>",
-    bos_token="<BOS>",
-    eos_token="<EOS>",
-    pad_token="<PAD>",
-    extra_ids=0,
-    legacy=False
-)
+        tokenizer1.save_pretrained(OUTPUT_DIR_1)
 
-tokenizer.save_pretrained(OUTPUT_DIR)
+    if save_as_t5_tokenizer:
+        j = 1
+        while os.path.exists(OUTPUT_DIR_2):
+            OUTPUT_DIR_2 = f"models/custom_t5_tokenizer_{j}"
+            j += 1
+        os.makedirs(OUTPUT_DIR_2)
+        
+        tokenizer2 = T5Tokenizer(
+            vocab_file=MODEL_PREFIX+".model",
+            unk_token="<UNK>",
+            bos_token="<BOS>",
+            eos_token="<EOS>",
+            pad_token="<PAD>",
+            extra_ids=0,
+            legacy=False
+        )
+
+        tokenizer2.save_pretrained(OUTPUT_DIR_2)
+
+
+if __name__ == "__main__":
+    user_defined_symbols = [
+        "<CONTEXT>",
+        "</CONTEXT>",
+        "<ROLE>",
+        "</ROLE>",
+        "User",
+        "Aku",
+        "😊",
+        "🥰",
+        "😉",
+        "🤗",
+        "😭",
+        "🤣",
+        "😆",
+        "🙄",
+        "😡",
+        "😲",
+        "😳",
+        "😔",
+        "😇",
+        "😙",
+        "🥳",
+        "🤔",
+        "🥺",
+        "🥱",
+        "💓",
+        "✨",
+        "<ATTR>",
+        "</ATTR>",
+        "likability",
+        "mood",
+        "\n",
+        " ",
+        "<SEP>"
+    ]
+
+    train_tokenizer(save_as_t5_tokenizer=False)
