4b208004efd78ca0fdeb8a3c2108308dbc6c8b08
YukiTomita-CC <tomita_y@clowncolor.com>
2024-10-11 03:55:22 +0000

    temp

--- aku/fine_tuning/train_dpo.py
+++ aku/fine_tuning/train_dpo.py
@@ -1,10 +1,10 @@
 from datasets import load_dataset
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
-from trl import DPOTrainer
+from trl import DPOTrainer, DPOConfig
 
 
-dataset = load_dataset("YukiTomita-CC/AKU-d_ms-0.5B-chat-v0.1_dpo_dataset")
+dataset = load_dataset("YukiTomita-CC/AKU-d_ms-0.5B-chat-v0.1_dpo_dataset", token="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")
 
 train_dataset = dataset["train"]
 val_dataset = dataset["test"]
@@ -17,21 +17,21 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
 )
 
-training_args = TrainingArguments(
+training_args = DPOConfig(
     output_dir="aku/fine_tuning/output_dpo",
     do_train=True,
     do_eval=True,
     evaluation_strategy="steps",
     prediction_loss_only=True,
-    per_device_train_batch_size=1,
-    per_device_eval_batch_size=1,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
     gradient_accumulation_steps=1,
     learning_rate=1e-6,
-    weight_decay=0.1,
+    weight_decay=0.01,
     adam_beta1=0.9,
     adam_beta2=0.95,
     adam_epsilon=1.0e-4,
-    num_train_epochs=5,
+    num_train_epochs=2,
     lr_scheduler_type="cosine",
     logging_dir="aku/fine_tuning/logs",
     logging_strategy="steps",
@@ -44,7 +44,7 @@ training_args = TrainingArguments(
     fp16=True,
     eval_steps=1,
     dataloader_num_workers=4,
-    run_name="test",
+    run_name="inc_epoch",
     remove_unused_columns=True,
     optim="adamw_bnb_8bit",
     report_to="wandb",
@@ -60,7 +60,7 @@ dpo_trainer = DPOTrainer(
     max_prompt_length=187,
     max_length=512,
     loss_type="sigmoid",
-    label_smoothing=0.0,
+    label_smoothing=0.1,
     precompute_ref_log_probs=True,
 )
 
